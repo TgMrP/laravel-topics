f811469e141717606205ff5a09c6abfe565768d4
Ran Tayar <tgmrp.dev@gmail.com>
2022-07-11 19:01:09 +0300

    update

--- config/app.php
+++ config/app.php
@@ -195,6 +195,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Ran Add
+         */
+        // Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
